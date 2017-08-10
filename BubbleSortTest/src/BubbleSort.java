dd4f898072d88f7c7df15418cbc5b347b7d0e072
Marcin <marcinster@gmail.com>
2017-08-10 20:56:19 +0200

    Add BubbleSortTest

---
+++ BubbleSortTest/src/BubbleSort.java
@@ -0,0 +1,30 @@
+
+public class BubbleSort {
+
+//    public BubbleSort(){
+//        int tab[] = new tab[];
+//        int temp;
+//
+//        for (int i = 0; i < tab.length ;i++) {
+//            tab[i+1];
+//        }
+//
+//        for (int i = 0; i < tab.length; i++) {
+//            for (int j = 0; j < tab.length; j++) {
+//                if (tab[j] < tab[j + 1]) {
+//                    temp = tab[j + 1];
+//                    tab[j] = tab[j + 1];
+//                    tab[j + 1] = temp;
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < tab.length; i++) {
+//            System.out.println(tab[i]);
+//        }
+//    }
+
+    public String[] bubbleSort(int n) {
+        return null;
+    }
+}
---
+++ BubbleSortTest/src/BubbleSortTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BubbleSortTest {
+
+    BubbleSort testObject = new BubbleSort();
+
+@Test
+
+    public void shouldReturnEmptyStringWhenEmptyStringProvided(){
+    Assert.assertArrayEquals(new int[], testObject);
+}
+
+}
