3340f89811cdda833bad48e20bbbdb20b77f61b6
Marcin <marcinster@gmail.com>
2017-08-10 20:11:56 +0200

    Add FizzBuzz

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -1,19 +1,24 @@
 public class FizzBuzz {
     public String[] fizzBuzz(int n) {
+        if (n < 0) return new String[0];
         String[] result = new String[n];
         for (int i = 1; i <= n; i++) {
-            transformElement(result, i);
+            result[i - 1] = getStringRepresentation(i);
         }
-//        if (n == 2) result = new String[]{"1", "2"};
-//        if (n == 1) result = new String[]{"1"};
         return result;
     }
 
-    private void transformElement(String[] result, int i) {
-        if (i == 3) {  //bo i +1
-            result[i - 1] = "Fizz";
-        } else {
-            result[i - 1] = (i) + "";
+    public String getStringRepresentation(int elementIndex) {
+        String result = "";
+        if (elementIndex % 3 == 0) {
+            result += "Fizz";
         }
+        if (elementIndex % 5 == 0) {
+            result += "Buzz";
+        }
+        if (elementIndex % 3 != 0 && elementIndex % 5 != 0) {
+            result += elementIndex;
+        }
+        return result;
     }
 }
--- FizzBuzz/src/FizzBuzzTest.java
+++ FizzBuzz/src/FizzBuzzTest.java
@@ -24,4 +24,35 @@ public class FizzBuzzTest {
     public void shouldReturnArraywithOnly1And2AndFizWhen3Provided() {
         Assert.assertArrayEquals(new String[]{"1", "2", "Fizz"}, testObject.fizzBuzz(3));
     }
+
+    @Test
+    public void shouldReturnArraywithBuzzWhen5Provided() {
+        Assert.assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz"}, testObject.fizzBuzz(5));
+    }
+
+    @Test
+    public void shouldReturnArraywith2Fizzon3rdAnd6thPositionWhen6Provided() {
+        Assert.assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz"}, testObject.fizzBuzz(6));
+    }
+
+    @Test
+    public void shouldReturnArraywith2BuzzWhen10Provided() {
+        Assert.assertArrayEquals(new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"},
+                testObject.fizzBuzz(10));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzWhenElementis15() {
+        Assert.assertEquals("FizzBuzz", testObject.getStringRepresentation(300));
+        //tu getstringrepresentation zamiast testObject()
+    }
+
+//    @Test
+//    public void shouldNotAcceptNegativeNumbers() {
+//        ("Fizz", testObject.getStringRepresentation(-5));
+//    }
+
+//    public void test (int input, String[] expectedOutput){
+//        Assert.assertArrayEquals(expectedOutput, testObject.fizzBuzz());
+//    }
 }
