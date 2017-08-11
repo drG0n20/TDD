aff8b9a05ed9a746c72a9e7e728a4863a0953732
Marcin <marcinster@gmail.com>
2017-08-11 19:57:45 +0200

    zmiany

--- KonverterRoman2/src/RomanConverter.java
+++ KonverterRoman2/src/RomanConverter.java
@@ -1,14 +1,14 @@
 public class RomanConverter {
     public String convert(int number) {
-        if(number == 1)return "Bleh";
         String result = "";
-        if(number == 4)return "IV";
-        if(number>=5) {
+        if (number == 4) return "IV";
+        if (number == 9) return "IX";
+        if (number >= 5) {
             result = "V";
-                    number -=5;
+            number -= 5;
         }
-        for (int i = 0; i <number ; i++) {
-            result +="I";
+        for (int i = 0; i < number; i++) {
+            result += "I";
         }
         return result;
     }
