c57a741f30f7f878b0f1f8866f5987c21f2d8de0
Marcin <marcinster@gmail.com>
2017-08-16 18:44:46 +0200

    created Vending Machine Maven project

--- VendingMachine/src/com/company/VendingMachineTest.java
+++ VendingMachine/src/com/company/VendingMachineTest.java
@@ -2,41 +2,14 @@ package com.company;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * VendingMachine
- *
- */
-
 public class VendingMachineTest {
-        VendingMachine vendingMachine = new VendingMachine();
 
         @Test
-        public void whenChoosingProductWithoutMoneyNothingHappens(){
-            String result = vendingMachine.execute("A");
-            Assert.assertEquals("",result);
+        public void nothing(){
+            Kasa kasa = null;
+            Produkty produkty = null;
+            VendingMachine vendingMachine = new VendingMachine(kasa, produkty);
         }
 
-        @Test
-        public void whenInsertNotEnoughMoneyAndPressProductGiveMoneyBack(){
-            String result = vendingMachine.execute("DA");
-            Assert.assertEquals("D",result);
-        }
 
-        @Test
-        public void whenInsertQarterIwantQarterBack(){
-            String result = vendingMachine.execute("QA");
-            Assert.assertEquals("Q",result);
-        }
-
-        @Test
-        public void whenInsertMoreThanOneCoinShouldReturnThemAllBack(){
-            String result = vendingMachine.execute("DQNdA");
-            Assert.assertEquals("DQNd",result);
-        }
-
-        @Test
-        public void whenClickAShouldGetAWithoutMoney(){
-            String result = vendingMachine.execute("AA");
-            Assert.assertEquals("",result);
-        }
 }
